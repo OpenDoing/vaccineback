9c84cf12d15119a0a29c7fe67bfb313b77547f1b
opeendoing <dyninger@163.com>
2019-03-19 21:44:45 +0800

    配置

---
+++ src/test/java/com/gyy/vaccine/VaccineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gyy.vaccine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaccineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
